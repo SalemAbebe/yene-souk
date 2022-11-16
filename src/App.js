89031d1e1f1b54e3772f056dc0939493cbc5e898
Salem Abebe <salemabebe@yahoo.com>
2022-11-16 14:31:31 -0500

    test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Test from "./components/test";
 
 function App() {
   return (
     <div className="App">
       <h1>hello, welcome to Yene-souk!!!</h1>
+      <Test />
     </div>
   );
 }
