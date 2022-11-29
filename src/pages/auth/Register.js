e264bb4b3d351e88e159a19bf8835993614d89e1
Salem Abebe <salemabebe@yahoo.com>
2022-11-29 12:36:00 -0500

    register page

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -3,8 +3,8 @@ import styles from "./Auth.module.scss";
 import register from "../../assets/images/register.png";
 import { Link, useNavigate } from "react-router-dom";
 import Card from "../../components/card/Card";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import Loader from "../../components/loader/Loader";
@@ -41,7 +41,6 @@ const Register = () => {
   };
   return (
     <>
-      <ToastContainer />
       {isLoading && <Loader />}
       <section className={`container ${styles.auth}`}>
         <Card>
