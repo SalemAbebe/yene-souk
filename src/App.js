4f148477539c54f74ab126a4c7285aa72efa147f
Salem Abebe <salemabebe@yahoo.com>
2022-11-29 12:39:24 -0500

    create login routes and add toast container

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import Cart from "./pages/cart/Cart";
 import Home from "./pages/home/Home";
 import Contact from "./pages/contact/Contact";
 import Footer from "./components/footer/Footer";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Reset from "./pages/auth/Reset";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <Router>
+        <ToastContainer />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
         </Routes>
         <Footer />
       </Router>
