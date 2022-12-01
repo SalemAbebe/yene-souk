90071fb969a4665c091fe2c215ba966aa844b25a
Salem Abebe <salemabebe@yahoo.com>
2022-12-01 10:02:20 -0500

    component created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./Home.module.scss";
 import AdminOnlyRoute from "../../components/adminOnlyRoute/AdminOnlyRoute";
+import Product from "../../components/product/Product";
 
 const Home = () => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setAllProducts(data));
+  }, []);
+  console.log(allProducts);
+
   return (
     <div>
       <h1>Home</h1>
+      <Product />
     </div>
   );
 };
