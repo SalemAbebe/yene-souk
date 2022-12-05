605699951088c0b6b0b9ffcc79c2e7020ed65560
Salem Abebe <salemabebe@yahoo.com>
2022-12-05 17:18:50 -0500

    product details page add

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,8 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProductDetails.module.scss";
+import { Link, useParams } from "react-router-dom";
+import Card from "../../card/Card";
+import { data } from "../../../data/data";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  // console.log(data);
+  // console.log(id);
+  console.log(typeof id);
+
+  useEffect(() => {
+    const detail = data.find((data) => {
+      if (data.id === id * 1) {
+        return data;
+      }
+    });
+    console.log(detail);
+    setProduct(detail);
+  }, [id]);
+  console.log(product);
+  return (
+    <section>
+      <div className={`container ${styles.product}`}>
+        <h2>Product Details</h2>
+        <div>
+          <Link to="/#products">&larr; Back To Products</Link>
+        </div>
+        {product === null ? (
+          <img
+            src={require("../../../assets/images/loader-spinning.gif")}
+            alt="Loading ..."
+            style={{ width: "50px" }}
+            className=" --center-all"
+          />
+        ) : (
+          <>
+            <div className={styles.details}>
+              <div className={styles.img}>
+                <img src={product.imageURL} alt={product.name} />
+              </div>
+              <div className={styles.content}>
+                <h3>{product.name}</h3>
+                <p className={styles.price}>{`$${product.price}`}</p>
+                <p>{product.desc}</p>
+                <p>
+                  <b>SKU</b> {id}
+                </p>
+                <p>{/* <b>Brand</b> {product.brand} */}</p>
+
+                {/* <div className={styles.count}>
+                  {isCartAdded < 0 ? null : (
+                    <>
+                      <button
+                        className="--btn"
+                        onClick={() => decreaseCart(product)}
+                      >
+                        -
+                      </button>
+                      <p>
+                        <b>{cart.cartQuantity}</b>
+                      </p>
+                      <button
+                        className="--btn"
+                        onClick={() => addToCart(product)}
+                      >
+                        +
+                      </button>
+                    </>
+                  )}
+                </div> */}
+                <button
+                  className="--btn --btn-danger"
+                  // onClick={() => addToCart(product)}
+                >
+                  ADD TO CART
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+        {/* <Card cardClass={styles.card}>
+          <h3>Product Reviews</h3>
+          <div>
+            {filteredReviews.length === 0 ? (
+              <p>There are no reviews for this product yet.</p>
+            ) : (
+              <>
+                {filteredReviews.map((item, index) => {
+                  const { rate, review, reviewDate, userName } = item;
+                  return (
+                    <div key={index} className={styles.review}>
+                      <StarsRating value={rate} />
+                      <p>{review}</p>
+                      <span>
+                        <b>{reviewDate}</b>
+                      </span>
+                      <br />
+                      <span>
+                        <b>by: {userName}</b>
+                      </span>
+                    </div>
+                  );
+                })}
+              </>
+            )}
+          </div>
+        </Card> */}
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetails;
