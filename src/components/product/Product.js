016a5d8976890c5b8e66559b281972d880c4f181
Salem Abebe <salemabebe@yahoo.com>
2022-12-05 11:07:27 -0500

    product component

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,19 +1,38 @@
-import React from "react";
-//
+import React, { useEffect, useState } from "react";
 import styles from "./Product.module.scss";
 import ProductFilter from "./productFilter/ProductFilter";
 import ProductList from "./productList/ProductList";
+import { data } from "../../data/data";
 
 const Product = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  // const [data, setData] = useState([]);
+  // const allData = () => {
+  //   axios.get("https://fakestoreapi.com/products").then((response) => {
+  //     console.log(response);
+  //     const myData = response.data;
+  //     setData(myData);
+  //   });
+  // };
+  // useEffect(() => allData, []);
+  console.log(data);
   return (
     <section>
       <div className={`container ${styles.product}`}>
         <aside className={styles.filter}>
           <ProductFilter />
-          <p className="test11">nkhjhjh</p>
         </aside>
         <div className={styles.content}>
-          <ProductList />
+          {isLoading ? (
+            <img
+              src={require("../../assets/images/loader-spinning.gif")}
+              alt="Loading ..."
+              style={{ width: "50px" }}
+              className=" --center-all"
+            />
+          ) : (
+            <ProductList products={data} />
+          )}
         </div>
       </div>
     </section>
