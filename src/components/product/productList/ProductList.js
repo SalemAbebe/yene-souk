2fdf203eb486fbfaa75c28ed12e3895f1b0a0890
Salem Abebe <salemabebe@yahoo.com>
2022-12-05 11:06:58 -0500

    product list component created

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,8 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProductList.module.scss";
+import ProductItem from "../productItem/ProductItem";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+// import Search from "../../search/Search";
+import { useDispatch, useSelector } from "react-redux";
+// import {
+//   FILTER_BY_SEARCH,
+//   selectFilteredProducts,
+//   SORT_PRODUCTS,
+// } from "../../../redux/slice/filterSlice";
+// import Pagination from "../../pagination/Pagination";
 
-const ProductList = () => {
-  return <div>ProductList</div>;
+const ProductList = ({ products }) => {
+  const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("latest");
+  // const filteredProducts = useSelector(selectFilteredProducts);
+
+  // Pagination states
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(9);
+  // Get Current Products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  // const currentProducts = filteredProducts.slice(
+  //   indexOfFirstProduct,
+  //   indexOfLastProduct
+  // );
+
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(SORT_PRODUCTS({ products, sort }));
+  // }, [dispatch, products, sort]);
+
+  // useEffect(() => {
+  //   dispatch(FILTER_BY_SEARCH({ products, search }));
+  // }, [dispatch, products, search]);
+
+  return (
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
+
+          <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
+
+          {/* <p>
+            <b>{filteredProducts.length}</b> Products found.
+          </p> */}
+        </div>
+        {/* Search Icon */}
+        {/* <div>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div> */}
+        {/* Sort Products */}
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        {products.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          <>
+            {" "}
+            {products.map((product) => {
+              return (
+                <div key={product.id}>
+                  <ProductItem {...product} product={product} grid={grid} />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
