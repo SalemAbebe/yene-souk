914bdc87b52020bfccbdcece399b861532d8f421
Salem Abebe <salemabebe@yahoo.com>
2022-12-09 09:31:03 -0500

    contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,97 @@
-import React from "react";
+import { useRef } from "react";
+import Card from "../../components/card/Card";
+import styles from "./Contact.module.scss";
+import { FaPhoneAlt, FaEnvelope, FaTwitter } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(form.current);
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        "template_5p9b133",
+        form.current,
+        "0LVgk2yRb3Jy3qoPU"
+      )
+      .then(
+        (result) => {
+          toast.success("Message sent successfully");
+        },
+        (error) => {
+          toast.error(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section>
+      <div className={`container ${styles.contact}`}>
+        <h2>Contact Us</h2>
+        <div className={styles.section}>
+          <form ref={form} onSubmit={sendEmail}>
+            <Card cardClass={styles.card}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Full Name"
+                required
+              />
+              <label>Email</label>
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Your active email"
+                required
+              />
+              <label>Subject</label>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+              <label>Message</label>
+              <textarea name="message" cols="30" rows="10"></textarea>
+              <button className="--btn --btn-primary">Send Message</button>
+            </Card>
+          </form>
+
+          <div className={styles.details}>
+            <Card cardClass={styles.card2}>
+              <h3>Our Contact Information</h3>
+              <p>Fill the form or contact us via other channels listed below</p>
+              <div className={styles.icons}>
+                <span>
+                  <FaPhoneAlt />
+                  <p>+1 (511) 746 8876</p>
+                </span>
+                <span>
+                  <FaEnvelope />
+                  <p>Support@yenesouk.com</p>
+                </span>
+                <span>
+                  <GoLocation />
+                  <p>GA, USA</p>
+                </span>
+                <span>
+                  <FaTwitter />
+                  <p>@yene-souk</p>
+                </span>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
